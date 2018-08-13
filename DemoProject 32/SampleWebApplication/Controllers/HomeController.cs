0bdffebb874c8177918f1d075c5fb31c3ea68018
ecwpz91 <ecwpz91@users.noreply.github.com>
2018-08-13 12:53:38 -0400

    Initial revision

--- DemoProject 32/SampleWebApplication/Controllers/HomeController.cs
+++ DemoProject 32/SampleWebApplication/Controllers/HomeController.cs
@@ -11,6 +11,36 @@ namespace SampleWebApplication.Controllers
 {
     public class HomeController : Controller
     {
+                public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
         public ActionResult Index()
         {
             List<Class1> ObjCustomer = new List<Class1>();
